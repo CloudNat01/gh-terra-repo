cf8d3620f18e542d459db5158babfab8c1710cc4
cloudnat <cloudnativist01@gmail.com>
2023-05-21 17:12:21 -0400

    combination

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance-type
   key_name      = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.github-act]
+  vpc_security_group_ids = [aws_security_group.github-act.id]
 
   tags = {
     Name = "${var.name}-server"
